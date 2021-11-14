2f997e9294ddd44b5ed80f99f0838cb5328bc7ca
amayaquilesantoniomanuel <91129425+amayaquilesantoniomanuel@users.noreply.github.com>
2021-11-14 21:20:26 +0100

    Add files via upload

---
+++ src/insert.js
@@ -0,0 +1,276 @@
+db.final.drop()
+db.final.insert([
+  {
+    jugador:
+    {
+      nombre: "lebron",
+      apellido: "james"
+    },
+    nacionalidad: "Estadounidense",
+    posicion: ["alero", "base", "ala-pivot"],
+    tiros_de_campo: "50,5",
+    altura: 206,
+    tiros_libres: "73,4",
+    mvp: true,
+    nacimiento: new Date(1984, 11, 30),
+    anillo:[true, 4]
+  },
+  {
+    jugador:
+    {
+      nombre: "stephen",
+      apellido: "curry"
+    },
+    nacionalidad: "Estadounidense",
+    posicion: ["base", "escolta"],
+    tiros_de_campo: "40,2",
+    altura: 191,
+    tiros_libres: "90,6",
+    mvp: true,
+    nacimiento: new Date(1988, 3, 14),
+    anillo:[true,3]
+  },
+  {
+    jugador:
+    {
+      nombre: "kevin",
+      apellido: "durant"
+    },
+    nacionalidad: "Estadounidense",
+    posicion: ["escolta", "alero", "ala-pivot"],
+    tiros_de_campo: "52,1",
+    altura: 208,
+    tiros_libres: "88,3",
+    mvp: true,
+    nacimiento: new Date(1988, 9, 29),
+    anillo:[true, 2]
+  },
+  {
+    jugador:
+    {
+      nombre: "giannis",
+      apellido: "antetokoumpo"
+    },
+    nacionalidad: "Greco",
+    posicion: ["base", "alero", "ala-pivot"],
+    tiros_de_campo: "55,3",
+    altura: 211,
+    tiros_libres: "72,2",
+    mvp: true,
+    nacimiento: new Date(1994, 12, 6),
+    anillo:[true , 1]
+  },
+  {
+    jugador:
+    {
+      nombre: "luka",
+      apellido: "doncic"
+    },
+    nacionalidad: "Esloveno",
+    posicion: ["escolta", "alero"],
+    tiros_de_campo: "46,3",
+    altura: 201,
+    tiros_libres: "73,7",
+    mvp: false,
+    nacimiento: new Date(1999, 2, 28),
+  },
+  {
+    jugador:
+    {
+      nombre: "Rudy",
+      apellido: "Gobert"
+    },
+    nacionalidad: "Frances",
+    posicion: "pivot",
+    tiros_de_campo: "69,3",
+    altura: 216,
+    tiros_libres: "63,1",
+    mvp: false,
+    nacimiento: new Date(1992, 6, 26),
+  },
+  {
+    jugador:
+    {
+      nombre: "klay",
+      apellido: "thompson"
+    },
+    nacionalidad: "Estadounidense",
+    posicion: ["escolta", "alero"],
+    tiros_de_campo: "46,7",
+    altura: 198,
+    tiros_libres: "84,8",
+    mvp: false,
+    nacimiento: new Date(1990, 2, 8),
+    anillo:[true, 3]
+  },
+  {
+    jugador:
+    {
+      nombre: "kyrie",
+      apellido: "irving"
+    },
+    nacionalidad: "Estadounidense",
+    posicion: "base",
+    tiros_de_campo: "47,8",
+    altura: 188,
+    tiros_libres: "87,7",
+    mvp: false,
+    nacimiento: new Date(1992, 3, 23),
+    anillo:[true, 1]
+  },
+  {
+    jugador:
+    {
+      nombre: "russel",
+      apellido: "westbrook"
+    },
+    nacionalidad: "Estadounidense",
+    posicion: ["base", "escolta"],
+    tiros_de_campo: "47,2",
+    altura: 191,
+    tiros_libres: "79",
+    mvp: true,
+    nacimiento: new Date(1988, 11, 12),
+  },
+  {
+    jugador:
+    {
+      nombre: "alex",
+      apellido: "caruso"
+    },
+    nacionalidad: "Estadounidense",
+    posicion: "escolta",
+    tiros_de_campo: "41,2",
+    altura: 196,
+    tiros_libres: "75",
+    mvp: false,
+    nacimiento: new Date(1994, 2, 28),
+    anillo:[true, 1]
+  },
+  {
+    jugador:
+    {
+      nombre: "anthony",
+      apellido: "davis"
+    },
+    nacionalidad: "Estadounidense",
+    posicion: ["ala-pivot", "pivot"],
+    tiros_de_campo: "50,3",
+    altura: 208,
+    tiros_libres: "80,2",
+    mvp: false,
+    nacimiento: new Date(1993, 3, 11),
+    anillo:[true, 1]
+  },
+  {
+    jugador:
+    {
+      nombre: "jason",
+      apellido: "tatum"
+    },
+    nacionalidad: "Estadounidense",
+    posicion: "alero",
+    tiros_de_campo: "45",
+    altura: 203,
+    tiros_libres: "82,9",
+    mvp: false,
+    nacimiento: new Date(1998, 3, 3),
+  },
+  {
+    jugador:
+    {
+      nombre: "kawhi",
+      apellido: "leonard"
+    },
+    nacionalidad: "Estadounidense",
+    posicion: ["escolta","alero", "ala-pivot"],
+    tiros_de_campo: "47",
+    altura: 201,
+    tiros_libres: "85,5",
+    mvp: false,
+    nacimiento: new Date(1991, 6, 29),
+    anillo:[true, 2]
+  },
+  {
+    jugador:
+    {
+      nombre: "paul",
+      apellido: "george"
+    },
+    nacionalidad: "Estadounidense",
+    posicion: ["escolta", "alero"],
+    tiros_de_campo: "43,9",
+    altura: 203,
+    tiros_libres: "84,6",
+    mvp: false,
+    nacimiento: new Date(1990, 5, 2),
+  },
+  {
+    jugador:
+    {
+      nombre: "kemba",
+      apellido: "walker"
+    },
+    nacionalidad: "Estadounidense",
+    posicion: "base",
+    tiros_de_campo: "42,5",
+    altura: 183,
+    tiros_libres: "83,7",
+    mvp: false,
+    nacimiento: new Date(1990, 5, 8),
+  },
+  {
+    jugador:
+    {
+      nombre: "ja",
+      apellido: "morant"
+    },
+    nacionalidad: "Estadounidense",
+    posicion: "base",
+    tiros_de_campo: "47,7",
+    altura: 191,
+    tiros_libres: "77,6",
+    mvp: false,
+    nacimiento: new Date(1999, 8, 10),
+  },
+  {
+    jugador:
+    {
+      nombre: "joel",
+      apellido: "embiid"
+    },
+    nacionalidad: "Camerunes",
+    posicion: "pivot",
+    tiros_de_campo: "47,7",
+    altura: 213,
+    tiros_libres: "79,3",
+    mvp: false,
+    nacimiento: new Date(1994, 3, 16),
+  },
+  {
+    jugador:
+    {
+      nombre: "ben",
+      apellido: "simmons"
+    }, nacionalidad: "Australiano",
+    posicion: ["base", "alero", "ala-pivot"],
+    tiros_de_campo: "58",
+    altura: 208,
+    tiros_libres: "59,3",
+    mvp: false,
+    nacimiento: new Date(1996, 7, 20),
+  },
+  {
+    jugador:
+    {
+      nombre: "james",
+      apellido: "harden"
+    }, nacionalidad: "Estadounidense",
+    posicion: ["base", "escolta"],
+    tiros_de_campo: "44.4",
+    altura: 196,
+    tiros_libres: "85,8",
+    mvp: true,
+    nacimiento: new Date(1989, 8, 26),
+  },
+])
